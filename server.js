a9f1857f453ef32054c1eca5f22455b688e23901
Steffen B <steffenbj@hotmail.com>
2020-01-24 22:38:19 +0100

    Added GET route

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+// Add node package express
 const express = require('express');
 const app = express();
+
+const mockUserData = [
+    {name: 'Steffen'},
+    {name: 'Maxime'}
+]
+
+app.get('/users', function (req, res){
+    res.json({
+        success:true,
+        message:'Got the users!',
+        users:mockUserData
+    })
+})
 
 app.listen(8000, function() {
     console.log("The app is running!")

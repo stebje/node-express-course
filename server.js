0f6ab116822f4fd13750d3bf8311eee227d9096a
Steffen B <steffenbj@hotmail.com>
2020-01-24 23:02:52 +0100

    Added the login POST route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Add node package express
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 const mockUserData = [
     {name: 'Steffen'},
@@ -22,6 +25,28 @@ app.get('/users/:id', function (req, res){
         message: 'Retrieved one user',
         user: req.params.id
     })
+});
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = 'steffenba';
+    const mockPassword = '1234';
+
+    if(username==mockUsername && password==mockPassword){
+        res.json({
+            success: true,
+            message: 'Login successful!',
+            token: 'this is where the token goes'
+        })
+    }
+    else {
+        res.json({
+            success: false,
+            message: 'Incorrect credentials!'
+        })
+    }
 });
 
 app.listen(8000, function() {
